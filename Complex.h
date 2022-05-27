7a8409ad13926f694ec05934aea987b1ca220efb
PaulFrankowski <94003153+PaulFrankowski@users.noreply.github.com>
2022-05-27 10:26:53 +0200

    Erstellen der Klasse und zugehörige Methoden

---
+++ Complex.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Complex
+{
+private:
+	double Realteil;
+	double Imaginärteil;
+
+public:
+	//allgemeiner Konstruktor:
+	Complex(double Realteil, double Imaginärteil);
+
+	//Setter- und Gettermethoden:
+	void setRealteil(double Realteil);
+	double getRealteil();
+
+	void setImaginärteil(double Imaginärteil);
+	double getImaginärteil();
+
+	//Operatorfunktionen:
+	Complex operator+(Complex& z);
+	Complex operator-(Complex& z);
+};
